515f16ce91c8b63c9f9307b350ba86278b12bd13
Nur Ahmad Khatim <61086300+naimackerman@users.noreply.github.com>
2021-05-20 22:47:17 +0800

    feat(soal2): revision on soal2b (#11)
    * revisi soal2b

--- soal2/soal2b.c
+++ soal2/soal2b.c
@@ -10,42 +10,24 @@
 
 void *factorial(void* arg)
 {
-    char hasil[1024];
-    char addword[1024];
-    char kali[] = "*";
     int *data = (int *)arg;
     int k = 1;
-    int cnt = 0;
     if (data[0] >= data[1]) {
         for (int a=data[0]; a>(data[0] - data[1]); a--) {
             k *= a;
-            sprintf(addword, "%d", a);
-            if (cnt > 0) {
-                strncat(hasil, kali, 1);
-            }
-            strncat(hasil, addword, 10);
-            cnt++;
         }
     }
     if (data[1] > data[0]) {
         for (int a=data[0]; a>=1; a--) {
             k *= a;
-            sprintf(addword, "%d", a);
-            if (cnt > 0) {
-                strncat(hasil, kali, 1);
-            }
-            strncat(hasil, addword, 10);
-            cnt++;
         }
     }
     if (data[0] == 0 || data[1] == 0) {
         k = 0;
-        sprintf(hasil, "%d", k);
     }
       
-    char *p = (char*)malloc(sizeof(char));
-    // *p = k;
-    strcpy(p, hasil);
+    int *p = (int*)malloc(sizeof(int));
+    *p = k;
 
     pthread_exit(p);
 }
@@ -59,6 +41,15 @@ void main()
 
     int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
     value = shmat(shmid, NULL, 0);
+
+    printf("Matrix Output A:\n");
+    for (int i=0; i<4; i++) {
+        for (int j=0; j<6; j++) {
+            printf("%d", (*value)[i][j]);
+            if (j < 5) printf(" ");
+            else printf("\n");
+        }
+    }
 
     printf("Input Matrix Baru:\n");
     for (int i=0; i<4; i++) {
@@ -88,8 +79,8 @@ void main()
         void *k;
         pthread_join(threads[i], &k);
 
-        char *p = (char *)k;
-        printf("%s", p);
+        int *p = (int *)k;
+        printf("%d", *p);
         if ((i + 1) % c == 0) printf("\n");
         else printf(" ");
     }
